eafc16f9c4e52a8b08b5fcb2490df5ba365db46d
Alifyz <alifyzdev@gmail.com>
2020-05-02 00:01:04 -0300

    add: simple one screen ios app

---
+++ AmRich/AmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmRich
+//
+//  Created by Alify F Pires on 27/04/20.
+//  Copyright © 2020 Alify F Pires. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
